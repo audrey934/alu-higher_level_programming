a97596d6f2ad75de449e314176edb8112cb95caf
Audrey Noella Hategekimana <a.hategekim@alustudent.com>
2026-04-01 13:30:27 +0200

    Refactor City model column definitions

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -2,16 +2,16 @@
 """
 Contains the class definition of a City
 """
-
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base  # Import Base from model_state
+from sqlalchemy.ext.declarative import declarative_base
+
 
 class City(Base):
     """
     Class that defines each city
     """
     __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
