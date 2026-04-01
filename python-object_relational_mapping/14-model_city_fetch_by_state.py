db9f1a5a35c198b863f8d41747ff72376a8aa186
Audrey Noella Hategekimana <a.hategekim@alustudent.com>
2026-04-01 13:18:18 +0200

    Refactor city fetch script with argument validation
    Added argument check and improved query sorting.

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
 """
-Script outputs all City objects from the database hbtn_0e_14_usa
+Script that prints all City objects from the database hbtn_0e_14_usa
 """
 
-
 import sys
-from model_state import Base, State
-from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
 
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
+        sys.exit(1)
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    user, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Connect to the MySQL server
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(user, password, db_name),
+        pool_pre_ping=True
+    )
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    st_cty = session.query(State, City).filter(State.id == City.state_id).all()
+    # Query: join State and City, sort by City.id
+    results = session.query(State, City).filter(State.id == City.state_id).order_by(City.id).all()
 
-    for state, city in st_cty:
+    # Display as: <state name>: (<city id>) <city name>
+    for state, city in results:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.close()
